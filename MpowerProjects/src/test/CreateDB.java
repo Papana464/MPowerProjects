6774044a527f05699084e165733928a690e39550
papana4664 <papana.464@gmail.com>
2018-05-29 14:08:38 -0400

    mpowerlogic Database created and tables inserted

--- MpowerProjects/src/test/CreateDB.java
+++ MpowerProjects/src/test/CreateDB.java
@@ -2,6 +2,7 @@ package test;
 
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 //import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -10,30 +11,48 @@ import java.sql.SQLException;
 
 public class CreateDB {
 	static String st= null;
-	Connection con=DBConnection.getDBConnection();
+	static Connection con=DBConnection.getDBConnection();
 	public static void main(String[] args) throws Exception {
 		CreateDB obj= new CreateDB();
 		obj.createDB();
 		obj.createTable();
 		}
 	public void createDB() throws SQLException{
-		Connection con=DBConnection.getDBConnection();
-		String st="CREATE DATABASE registration";
+		con=DBConnection.getDBConnection();
+		String st="CREATE DATABASE mpowerlogic";
 		PreparedStatement stmt= con.prepareStatement(st);
 		stmt.executeUpdate();
 		System.out.println("Database created successfully...");
 		
 	}
 	public void createTable() throws SQLException{
-		Connection con=DBConnection.getDBConnection();
-		String st1 = "CREATE TABLE REGISTRATION "  +
+		Connection con1=DriverManager.getConnection("jdbc:mysql://localhost:3306/mpowerlogic?verifyServerCertificate=false&useSSL=true", "root", "dntlvplz");
+		
+		String st2 = "CREATE TABLE task "  +
                 "(id INTEGER not NULL, " +
-                " first VARCHAR(255), " + 
-                " last VARCHAR(255), " + 
-                " age INTEGER, " + 
+                " task_name VARCHAR(255), " + 
+                " task_description VARCHAR(255), " + 
                 " PRIMARY KEY ( id ))";
-		PreparedStatement stmt1= con.prepareStatement(st1);
+		PreparedStatement stmt2= con1.prepareStatement(st2);
+		stmt2.executeUpdate();
+			String st1 = "CREATE TABLE consultants "  +
+                "(id INTEGER not NULL, " +
+                " name VARCHAR(255), " +
+                " PRIMARY KEY ( id ))";
+		PreparedStatement stmt1= con1.prepareStatement(st1);
 		stmt1.executeUpdate();
+		
+		String st3 = "CREATE TABLE consultants_task_status "  +
+                "(id INTEGER not NULL, " +
+				"consultant_id Integer,"+"INDEX Key_idx (consultant_id) VISIBLE,FOREIGN KEY (consultant_id) REFERENCES consultants (id) ON DELETE NO ACTION ON UPDATE NO ACTION,"+ 
+                " task_id Integer, " + "INDEX Key_idx1 (task_id) VISIBLE,FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE NO ACTION ON UPDATE NO ACTION,"+
+				"task_start_time Integer,"+
+				"task_end_time Integer,"+
+				"task_duration Integer,"+
+				"grade_achieved VARCHAR(15),"+
+                " PRIMARY KEY ( id ))";
+		PreparedStatement stmt3= con1.prepareStatement(st3);
+		stmt3.executeUpdate();
 	}
 
 }
